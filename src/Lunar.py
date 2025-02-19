ecd30b61853c215548f6d64792d76c96dffb7aaf
Syconn <49461127+Syconn@users.noreply.github.com>
2025-02-19 10:57:44 -0500

    Compiler

--- src/Lunar.py
+++ src/Lunar.py
@@ -1,18 +1,9 @@
 # Compiler
 from Lexer import tokenize_line
-from Parser import *
+from Compiler import generate_code
+from Parser import parse
 
-def generate_code(ast): # C Based
-    c_code = "#include <stdio.h>\nint main() {\n"
-    for stmt in ast:
-        if isinstance(stmt, Print):
-            expr = stmt.expression
-            if isinstance(expr, BinaryOp):
-                c_code += f"    printf(\"%d\\n\", {expr.left.value} {expr.op} {expr.right.value});\n"
-            else:
-                c_code += f"    printf(\"%d\\n\", {expr.value});\n"
-    c_code += "    return 0;\n}"
-    return c_code
+
 
 tokens = tokenize_line("print(5 + 8); // Hey Im a Comment")
 ast = parse(tokens)
